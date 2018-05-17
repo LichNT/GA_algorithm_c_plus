c85c3fbb89ddc441176e629d7b8e2512a4bfec4b
Nguyen Tran Lich <usbusbubs@gmail.com>
2018-05-17 15:49:53 +0700

    update

--- GA.cpp
+++ GA.cpp
@@ -8,9 +8,6 @@
 # include <cstring>
 
 using namespace std;
-// 
-//  Change any of these parameters to match your needs 
-//
 # define POPSIZE 50
 # define MAXGENS 1000
 # define NVARS 3
@@ -52,62 +49,7 @@ void selector ( int &seed );
 void timestamp ( );
 void Xover ( int one, int two, int &seed );
 
-//****************************************************************************80
-
 int main ( )
-
-//****************************************************************************80
-//
-//  Purpose:
-//
-//    MAIN supervises the genetic algorithm.
-//
-//  Discussion:
-//
-//    Each generation involves selecting the best 
-//    members, performing crossover & mutation and then 
-//    evaluating the resulting population, until the terminating 
-//    condition is satisfied   
-//
-//    This is a simple genetic algorithm implementation where the 
-//    evaluation function takes positive values only and the 
-//    fitness of an individual is the same as the value of the 
-//    objective function.  
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    28 April 2014
-//
-//  Author:
-//
-//    Original version by Dennis Cormier and Sita Raghavan.
-//    This C++ version by John Burkardt.
-//
-//  Reference:
-//
-//    Zbigniew Michalewicz,
-//    Genetic Algorithms + Data Structures = Evolution Programs,
-//    Third Edition,
-//    Springer, 1996,
-//    ISBN: 3-540-60676-9,
-//    LC: QA76.618.M53.
-//
-//  Parameters:
-//
-//    MAXGENS is the maximum number of generations.
-//
-//    NVARS is the number of problem variables.
-//
-//    PMUTATION is the probability of mutation.
-//
-//    POPSIZE is the population size. 
-//
-//    PXOVER is the probability of crossover.                          
-//
 {
   string filename = "simple_ga_input.txt";
   int generation;
@@ -156,9 +98,6 @@ int main ( )
 
   cout << "\n";
   cout << "  Best fitness = " << population[POPSIZE].fitness << "\n";
-//
-//  Terminate.
-//
   cout << "\n";
   cout << "SIMPLE_GA:\n";
   cout << "  Normal end of execution.\n";
@@ -167,37 +106,8 @@ int main ( )
 
   return 0;
 }
-//****************************************************************************80
 
-void crossover ( int &seed )
-
-//****************************************************************************80
-// 
-//  Purpose:
-//
-//    CROSSOVER selects two parents for the single point crossover.
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    28 April 2014
-//
-//  Author:
-//
-//    Original version by Dennis Cormier and Sita Raghavan.
-//    This C++ version by John Burkardt.
-//
-//  Local parameters:
-//
-//    Local, int FIRST, is a count of the number of members chosen.
-//
-//  Parameters:
-//
-//    Input/output, int &SEED, a seed for the random number generator.
-//
+void crossover ( int &seed )// chon lua cha me de lai ghep
 {
   const double a = 0.0;
   const double b = 1.0;
@@ -227,43 +137,8 @@ void crossover ( int &seed )
   }
   return;
 }
-//****************************************************************************80
 
-void elitist ( )
-
-//****************************************************************************80
-// 
-//  Purpose:
-//
-//    ELITIST stores the best member of the previous generation.
-//
-//  Discussion:
-//
-//    The best member of the previous generation is stored as 
-//    the last in the array. If the best member of the current 
-//    generation is worse then the best member of the previous 
-//    generation, the latter one would replace the worst member 
-//    of the current population.
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    29 December 2007
-//
-//  Author:
-//
-//    Original version by Dennis Cormier and Sita Raghavan.
-//    This C++ version by John Burkardt.
-//
-//  Local parameters:
-//
-//    Local, double BEST, the best fitness value.
-//
-//    Local, double WORST, the worst fitness value.
-//
+void elitist ( )// luu tru lai nhung thanh vien tot nhat sau moi the he
 {
   int i;
   double best;
@@ -310,13 +185,6 @@ void elitist ( )
     }
 
   }
-// 
-//  If the best individual from the new population is better than 
-//  the best individual from the previous population, then 
-//  copy the best from the new population; else replace the 
-//  worst individual from the current population with the 
-//  best one from the previous generation                     
-//
   if ( population[POPSIZE].fitness <= best )
   {
     for ( i = 0; i < NVARS; i++ )
@@ -336,34 +204,8 @@ void elitist ( )
 
   return;
 }
-//****************************************************************************80
 
-void evaluate ( )
-
-//****************************************************************************80
-// 
-//  Purpose:
-//
-//    EVALUATE implements the user-defined valuation function
-//
-//  Discussion:
-//
-//    Each time this is changed, the code has to be recompiled.
-//    The current function is:  x[1]^2-x[1]*x[2]+x[3]
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    29 December 2007
-//
-//  Author:
-//
-//    Original version by Dennis Cormier and Sita Raghavan.
-//    This C++ version by John Burkardt.
-//
+void evaluate ( )// thuc hien danh gia sau tung the he 
 {
   int member;
   int i;
@@ -379,71 +221,8 @@ void evaluate ( )
   }
   return;
 }
-//****************************************************************************80
 
-int i4_uniform_ab ( int a, int b, int &seed )
-
-//****************************************************************************80
-//
-//  Purpose:
-//
-//    I4_UNIFORM_AB returns a scaled pseudorandom I4 between A and B.
-//
-//  Discussion:
-//
-//    The pseudorandom number should be uniformly distributed
-//    between A and B.
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    02 October 2012
-//
-//  Author:
-//
-//    John Burkardt
-//
-//  Reference:
-//
-//    Paul Bratley, Bennett Fox, Linus Schrage,
-//    A Guide to Simulation,
-//    Second Edition,
-//    Springer, 1987,
-//    ISBN: 0387964673,
-//    LC: QA76.9.C65.B73.
-//
-//    Bennett Fox,
-//    Algorithm 647:
-//    Implementation and Relative Efficiency of Quasirandom
-//    Sequence Generators,
-//    ACM Transactions on Mathematical Software,
-//    Volume 12, Number 4, December 1986, pages 362-376.
-//
-//    Pierre L'Ecuyer,
-//    Random Number Generation,
-//    in Handbook of Simulation,
-//    edited by Jerry Banks,
-//    Wiley, 1998,
-//    ISBN: 0471134031,
-//    LC: T57.62.H37.
-//
-//    Peter Lewis, Allen Goodman, James Miller,
-//    A Pseudo-Random Number Generator for the System/360,
-//    IBM Systems Journal,
-//    Volume 8, Number 2, 1969, pages 136-143.
-//
-//  Parameters:
-//
-//    Input, int A, B, the limits of the interval.
-//
-//    Input/output, int &SEED, the "seed" value, which should NOT be 0.
-//    On output, SEED has been updated.
-//
-//    Output, int I4_UNIFORM, a number between A and B.
-//
+int i4_uniform_ab ( int a, int b, int &seed )// random cac the he sinh ra trong quan the
 {
   int c;
   const int i4_huge = 2147483647;
@@ -458,9 +237,6 @@ int i4_uniform_ab ( int a, int b, int &seed )
     cerr << "  Input value of SEED = 0.\n";
     exit ( 1 );
   }
-//
-//  Guarantee A <= B.
-//
   if ( b < a )
   {
     c = a;
@@ -478,18 +254,9 @@ int i4_uniform_ab ( int a, int b, int &seed )
   }
 
   r = ( float ) ( seed ) * 4.656612875E-10;
-//
-//  Scale R to lie between A-0.5 and B+0.5.
-//
   r = ( 1.0 - r ) * ( ( float ) a - 0.5 ) 
     +         r   * ( ( float ) b + 0.5 );
-//
-//  Use rounding to convert R to an integer between A and B.
-//
   value = round ( r );
-//
-//  Guarantee A <= VALUE <= B.
-//
   if ( value < a )
   {
     value = a;
@@ -501,47 +268,8 @@ int i4_uniform_ab ( int a, int b, int &seed )
 
   return value;
 }
-//****************************************************************************80
 
-void initialize ( string filename, int &seed )
-
-//****************************************************************************80
-// 
-//  Purpose:
-//
-//    INITIALIZE initializes the genes within the variables bounds. 
-//
-//  Discussion:
-//
-//    It also initializes (to zero) all fitness values for each
-//    member of the population. It reads upper and lower bounds 
-//    of each variable from the input file `gadata.txt'. It 
-//    randomly generates values between these bounds for each 
-//    gene of each genotype in the population. The format of 
-//    the input file `gadata.txt' is 
-//
-//      var1_lower_bound var1_upper bound
-//      var2_lower_bound var2_upper bound ...
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    28 April 2014
-//
-//  Author:
-//
-//    Original version by Dennis Cormier and Sita Raghavan.
-//    This C++ version by John Burkardt.
-//
-//  Parameters:
-//
-//    Input, string FILENAME, the name of the input file.
-//
-//    Input/output, int &SEED, a seed for the random number generator.
-//
+void initialize ( string filename, int &seed )// khoi tao gen nguon cua cac the he
 {
   int i;
   ifstream input;
@@ -558,9 +286,6 @@ void initialize ( string filename, int &seed )
     cerr << "  Cannot open the input file!\n";
     exit ( 1 );
   }
-// 
-//  Initialize variables within the bounds 
-//
   for ( i = 0; i < NVARS; i++ )
   {
     input >> lbound >> ubound;
@@ -580,38 +305,8 @@ void initialize ( string filename, int &seed )
 
   return;
 }
-//****************************************************************************80
 
-void keep_the_best ( )
-
-//****************************************************************************80
-// 
-//  Purpose:
-//
-//    KEEP_THE_BEST keeps track of the best member of the population. 
-//
-//  Discussion:
-//
-//    Note that the last entry in the array Population holds a 
-//    copy of the best individual.
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    29 December 2007
-//
-//  Author:
-//
-//    Original version by Dennis Cormier and Sita Raghavan.
-//    This C++ version by John Burkardt.
-//
-//  Local parameters:
-//
-//    Local, int CUR_BEST, the index of the best individual.
-//
+void keep_the_best ( )// kiem tha, tim kiem va bo qua nhung doi tuong tot sau moi chu ky
 {
   int cur_best;
   int mem;
@@ -627,9 +322,6 @@ void keep_the_best ( )
       population[POPSIZE].fitness = population[mem].fitness;
     }
   }
-// 
-//  Once the best member in the population is found, copy the genes.
-//
   for ( i = 0; i < NVARS; i++ )
   {
     population[POPSIZE].gene[i] = population[cur_best].gene[i];
@@ -637,38 +329,8 @@ void keep_the_best ( )
 
   return;
 }
-//****************************************************************************80
 
-void mutate ( int &seed )
-
-//****************************************************************************80
-// 
-//  Purpose:
-//
-//    MUTATE performs a random uniform mutation. 
-//
-//  Discussion:
-//
-//    A variable selected for mutation is replaced by a random value 
-//    between the lower and upper bounds of this variable.
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    28 April 2014
-//
-//  Author:
-//
-//    Original version by Dennis Cormier and Sita Raghavan.
-//    This C++ version by John Burkardt.
-//
-//  Parameters:
-//
-//    Input/output, int &SEED, a seed for the random number generator.
-//
+void mutate ( int &seed )// tao dot bien cho tung the he
 {
   const double a = 0.0;
   const double b = 1.0;
@@ -694,42 +356,8 @@ void mutate ( int &seed )
 
   return;
 }
-//****************************************************************************80
 
-double r8_uniform_ab ( double a, double b, int &seed )
-
-//****************************************************************************80
-//
-//  Purpose:
-//
-//    R8_UNIFORM_AB returns a scaled pseudorandom R8.
-//
-//  Discussion:
-//
-//    The pseudorandom number should be uniformly distributed
-//    between A and B.
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    09 April 2012
-//
-//  Author:
-//
-//    John Burkardt
-//
-//  Parameters:
-//
-//    Input, double A, B, the limits of the interval.
-//
-//    Input/output, int &SEED, the "seed" value, which should NOT be 0.
-//    On output, SEED has been updated.
-//
-//    Output, double R8_UNIFORM_AB, a number strictly between A and B.
-//
+double r8_uniform_ab ( double a, double b, int &seed )// giao phoi ngau nhien 
 {
   int i4_huge = 2147483647;
   int k;
@@ -758,43 +386,8 @@ double r8_uniform_ab ( double a, double b, int &seed )
 
   return value;
 }
-//****************************************************************************80
 
-void report ( int generation )
-
-//****************************************************************************80
-// 
-//  Purpose:
-//
-//    REPORT reports progress of the simulation. 
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    29 December 2007
-//
-//  Author:
-//
-//    Original version by Dennis Cormier and Sita Raghavan.
-//    This C++ version by John Burkardt.
-//
-//  Local parameters:
-//
-//    Local, double avg, the average population fitness.
-//
-//    Local, best_val, the best population fitness.
-//
-//    Local, double square_sum, square of sum for std calc.
-//
-//    Local, double stddev, standard deviation of population fitness.
-//
-//    Local, double sum, the total population fitness.
-//
-//    Local, double sum_square, sum of squares for std calc.
-//
+void report ( int generation )// bao cao ket qua sau moi chu ky 
 {
   double avg;
   double best_val;
@@ -835,36 +428,7 @@ void report ( int generation )
 }
 //****************************************************************************80
 
-void selector ( int &seed )
-
-//****************************************************************************80
-// 
-//  Purpose:
-//
-//    SELECTOR is the selection function.
-//
-//  Discussion:
-//
-//    Standard proportional selection for maximization problems incorporating 
-//    the elitist model.  This makes sure that the best member always survives.
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    28 April 2014
-//
-//  Author:
-//
-//    Original version by Dennis Cormier and Sita Raghavan.
-//    This C++ version by John Burkardt.
-//
-//  Parameters:
-//
-//    Input/output, int &SEED, a seed for the random number generator.
-//
+void selector ( int &seed )// thuc hien hcon lua doi utong lai gheps
 {
   const double a = 0.0;
   const double b = 1.0;
@@ -873,33 +437,22 @@ void selector ( int &seed )
   int mem;
   double p;
   double sum;
-//
-//  Find the total fitness of the population.
-//
+
   sum = 0.0;
   for ( mem = 0; mem < POPSIZE; mem++ )
   {
     sum = sum + population[mem].fitness;
   }
-//
-//  Calculate the relative fitness of each member.
-//
   for ( mem = 0; mem < POPSIZE; mem++ )
   {
     population[mem].rfitness = population[mem].fitness / sum;
   }
-// 
-//  Calculate the cumulative fitness.
-//
   population[0].cfitness = population[0].rfitness;
   for ( mem = 1; mem < POPSIZE; mem++ )
   {
     population[mem].cfitness = population[mem-1].cfitness +       
       population[mem].rfitness;
   }
-// 
-//  Select survivors using cumulative fitness. 
-//
   for ( i = 0; i < POPSIZE; i++ )
   { 
     p = r8_uniform_ab ( a, b, seed );
@@ -918,9 +471,6 @@ void selector ( int &seed )
       }
     }
   }
-// 
-//  Overwrite the old population with the new one.
-//
   for ( i = 0; i < POPSIZE; i++ )
   {
     population[i] = newpopulation[i]; 
@@ -928,32 +478,8 @@ void selector ( int &seed )
 
   return;     
 }
-//****************************************************************************80
 
 void timestamp ( )
-
-//****************************************************************************80
-//
-//  Purpose:
-//
-//    TIMESTAMP prints the current YMDHMS date as a time stamp.
-//
-//  Example:
-//
-//    May 31 2001 09:45:54 AM
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    04 October 2003
-//
-//  Author:
-//
-//    John Burkardt
-//
 {
 # define TIME_SIZE 40
 
@@ -972,50 +498,14 @@ void timestamp ( )
   return;
 # undef TIME_SIZE
 }
-//****************************************************************************80
 
-void Xover ( int one, int two, int &seed )
-
-//****************************************************************************80
-// 
-//  Purpose:
-//
-//    XOVER performs crossover of the two selected parents. 
-//
-//  Licensing:
-//
-//    This code is distributed under the GNU LGPL license. 
-//
-//  Modified:
-//
-//    28 April 2014
-//
-//  Author:
-//
-//    Original version by Dennis Cormier and Sita Raghavan.
-//    This C++ version by John Burkardt.
-//
-//  Local parameters:
-//
-//    Local, int point, the crossover point.
-//
-//  Parameters:
-//
-//    Input, int ONE, TWO, the indices of the two parents.
-//
-//    Input/output, int &SEED, a seed for the random number generator.
-//
+void Xover ( int one, int two, int &seed )//thu hien cheo hoa 2 me con 
 {
   int i;
   int point;
   double t;
-// 
-//  Select the crossover point.
-//
   point = i4_uniform_ab ( 0, NVARS - 1, seed );
-//
-//  Swap genes in positions 0 through POINT-1.
-//
+
   for ( i = 0; i < point; i++ )
   {
     t                       = population[one].gene[i];
